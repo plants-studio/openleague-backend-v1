f6493c1e3135bf33b58f69559b01216444b9f22f
Jaeseong-CS <shimjs8@naver.com>
2020-10-02 17:38:59 +0900

    feat: get league list

--- src/routes/api/v1/league/index.ts
+++ src/routes/api/v1/league/index.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
 import hold from './hold';
+import list from './list';
 import participate from './participate';
 
 const router = Router();
 
 router.use('/hold', hold);
+router.use('/list', list);
 router.use('/participate', participate);
 
 export default router;
---
+++ src/routes/api/v1/league/list.ts
@@ -0,0 +1,30 @@
+import { Router } from 'express';
+
+import games from '../../../../docs/games.json';
+import League from '../../../../models/League';
+
+const router = Router();
+
+router.post('/', async (req, res) => {
+  const { page: tp, limit: tl } = req.query;
+  if (!(tp && tl)) {
+    res.sendStatus(412);
+    return;
+  }
+  const page = Number.parseInt(tp!.toString(), 10);
+  const limit = Number.parseInt(tl!.toString(), 10);
+  let filter: Array<string> = req.body.game;
+  if (!filter) {
+    filter = games;
+  }
+  const list: Array<any> = [];
+  await Promise.all(
+    filter.map(async (game) => {
+      const result = await League.paginate({ game }, { page, limit });
+      list.push(result.docs);
+    }),
+  );
+  res.status(200).send(list);
+});
+
+export default router;
