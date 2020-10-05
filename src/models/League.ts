2e5b93c2f55bfcda2125b89c5304be31b3bfc8fc
Jaeseong-CS <shimjs8@naver.com>
2020-10-05 21:01:25 +0900

    refactor: type

--- src/models/League.ts
+++ src/models/League.ts
@@ -4,7 +4,7 @@ import mongoosePaginate from 'mongoose-paginate-v2';
 const League = new Schema({
   title: String,
   content: String,
-  host: String,
+  host: Schema.Types.ObjectId,
   game: String,
   max: Number,
   member: [Schema.Types.ObjectId],
@@ -18,7 +18,7 @@ export default model('League', League);
 export interface ILeague extends Document {
   title?: string;
   content?: string;
-  host?: string;
+  host?: Schema.Types.ObjectId;
   game?: string;
   max?: number;
   member?: [Schema.Types.ObjectId];
