d56419a16a93d2524a5a69bb441b3acac4aa045a
Jaeseong-CS <shimjs8@naver.com>
2020-10-08 09:43:06 +0900

    feat: remove league

--- src/routes/api/v1/league/index.ts
+++ src/routes/api/v1/league/index.ts
@@ -3,11 +3,13 @@ import { Router } from 'express';
 import hold from './hold';
 import list from './list';
 import participate from './participate';
+import remove from './remove';
 
 const router = Router();
 
 router.use('/hold', hold);
 router.use('/list', list);
 router.use('/participate', participate);
+router.use('/', remove);
 
 export default router;
