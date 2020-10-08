0ca2e771fec59c139b02635e50032602631fcc52
Jaeseong-CS <shimjs8@naver.com>
2020-10-08 09:44:07 +0900

    feat: remove friends

--- src/routes/api/v1/user/friends/index.ts
+++ src/routes/api/v1/user/friends/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import add from './add';
+import remove from './remove';
 
 const router = Router();
 
-router.use('/add', add);
+router.use('/', add);
+router.use('/', remove);
 
 export default router;
