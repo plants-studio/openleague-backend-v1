effd626724ff7d3c25dcd5398bcdc7bdc0e13506
Jaeseong-CS <shimjs8@naver.com>
2020-10-08 22:09:05 +0900

    feat: team model

---
+++ src/models/Team.ts
@@ -0,0 +1,24 @@
+import { Document, model, Schema } from 'mongoose';
+import mongoosePaginate from 'mongoose-paginate-v2';
+
+const Team = new Schema({
+  name: String,
+  introduce: String,
+  leader: Schema.Types.ObjectId,
+  member: [Schema.Types.ObjectId],
+  isPublic: Boolean,
+  waiting: [Schema.Types.ObjectId],
+});
+
+Team.plugin(mongoosePaginate);
+
+export default model('Team', Team);
+
+export interface ITeam extends Document {
+  name?: string;
+  introduce?: string;
+  leader?: Schema.Types.ObjectId;
+  member?: [Schema.Types.ObjectId];
+  isPublic?: boolean;
+  waiting?: [Schema.Types.ObjectId];
+}
