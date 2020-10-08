f434a1c5e5f90dcd3c2d3a86f9ebbcfddcc91442
Jaeseong-CS <shimjs8@naver.com>
2020-10-08 22:24:49 +0900

    fix: typo

--- src/routes/api/v1/team/index.ts
+++ src/routes/api/v1/team/index.ts
@@ -1,19 +1,19 @@
 import { Router } from 'express';
 
 // import accept from './accept';
-// import create from './create';
+import create from './create';
 import join from './join';
 import list from './list';
-import remove from './remove';
+// import remove from './remove';
 // import waiting from './waiting';
 
 const router = Router();
 
 // router.use('/accept', accept);
-// router.use('/', create);
+router.use('/', create);
 router.use('/', join);
 router.use('/', list);
-router.use('/', remove);
+// router.use('/', remove);
 // router.use('/waiting', waiting);
 
 export default router;
