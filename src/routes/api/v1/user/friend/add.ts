c70b0d6f82d6576833f1f340759ca4c247ea926d
Jaeseong-CS <shimjs8@naver.com>
2020-10-10 14:37:24 +0900

    fix: id utilization

--- src/routes/api/v1/user/friend/add.ts
+++ src/routes/api/v1/user/friend/add.ts
@@ -5,22 +5,22 @@ import Friend, { IFriend } from '../../../../../models/Friend';
 
 const router = Router();
 
-router.put('/', auth, async (req: IRequest, res) => {
+router.put('/:id', auth, async (req: IRequest, res) => {
   const { token }: IToken = req;
-  const { target } = req.body;
-  if (!target) {
+  const { id } = req.params;
+  if (!id) {
     res.sendStatus(412);
     return;
   }
 
-  const myFriend: IFriend = await Friend.findById(token?.user?._id);
-  const targetFriend: IFriend = await Friend.findById(target);
+  const myFriend: IFriend = await Friend.findOne({ user: token?.user?._id });
+  const targetFriend: IFriend = await Friend.findOne({ user: id });
   if (!(myFriend && targetFriend)) {
     res.sendStatus(404);
     return;
   }
 
-  if (myFriend.applying?.find(target)) {
+  if (myFriend.applying?.find(id as any)) {
     res.sendStatus(409);
     return;
   }
@@ -30,14 +30,14 @@ router.put('/', auth, async (req: IRequest, res) => {
       $pull: { applying: token?.user?._id },
     });
     await myFriend.updateOne({
-      $push: { friends: target },
-      $pull: { waiting: target },
+      $push: { friends: id },
+      $pull: { waiting: id },
     });
     res.status(200).send('친구 신청을 수락했습니다.');
     return;
   }
 
-  await myFriend.updateOne({ $push: { applying: target } });
+  await myFriend.updateOne({ $push: { applying: id } });
   await targetFriend.updateOne({ $push: { waiting: token?.user?._id } });
   res.status(200).send('친구 신청을 보냈습니다.');
 });
