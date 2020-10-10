030ebe8d9fb49e7c401e6ac673f3c9829854a523
Jaeseong-CS <shimjs8@naver.com>
2020-10-10 13:17:30 +0900

    refactor: remove unused api

--- src/routes/api/v1/user/friends/index.ts
+++ src/routes/api/v1/user/friends/index.ts
@@ -3,13 +3,11 @@ import { Router } from 'express';
 import add from './add';
 import list from './list';
 import remove from './remove';
-// import waiting from './waiting';
 
 const router = Router();
 
 router.use('/', add);
 router.use('/', list);
 router.use('/', remove);
-// router.use('/waiting', waiting);
 
 export default router;
