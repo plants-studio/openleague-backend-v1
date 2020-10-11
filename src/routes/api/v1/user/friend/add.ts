7ea7d45066427b7a46e7125b7100a2f0cb0b84e7
Jaeseong-CS <shimjs8@naver.com>
2020-10-11 21:26:40 +0900

    fix: change from _id search to name search

--- src/routes/api/v1/user/friend/add.ts
+++ src/routes/api/v1/user/friend/add.ts
@@ -2,25 +2,32 @@ import { Router } from 'express';
 
 import auth, { IRequest, IToken } from '../../../../../middlewares/auth';
 import Friend, { IFriend } from '../../../../../models/Friend';
+import User, { IUser } from '../../../../../models/User';
 
 const router = Router();
 
-router.put('/:id', auth, async (req: IRequest, res) => {
+router.put('/:name', auth, async (req: IRequest, res) => {
   const { token }: IToken = req;
-  const { id } = req.params;
-  if (!id) {
+  const { name } = req.params;
+  if (!name) {
     res.sendStatus(412);
     return;
   }
 
-  const myFriend: IFriend = await Friend.findOne({ user: token?.user?._id });
-  const targetFriend: IFriend = await Friend.findOne({ user: id });
-  if (!(myFriend && targetFriend)) {
-    res.sendStatus(404);
+  const target: IUser = await User.findOne({ name });
+  if (!target) {
+    res.status(404).send('유저를 찾을 수 없습니다');
     return;
   }
 
-  if (myFriend.applying?.find(id as any)) {
+  const myFriend: IFriend = await Friend.findOne({ user: token?.user?._id });
+  const targetFriend: IFriend = await Friend.findOne({ user: target._id });
+  if (!(myFriend && targetFriend)) {
+    res.status(404).send('친구 데이터를 찾을 수 없습니다');
+    return;
+  }
+
+  if (myFriend.applying?.find(target._id as any)) {
     res.sendStatus(409);
     return;
   }
@@ -30,14 +37,14 @@ router.put('/:id', auth, async (req: IRequest, res) => {
       $pull: { applying: token?.user?._id },
     });
     await myFriend.updateOne({
-      $push: { friends: id },
-      $pull: { waiting: id },
+      $push: { friends: target._id },
+      $pull: { waiting: target._id },
     });
     res.status(200).send('친구 신청을 수락했습니다.');
     return;
   }
 
-  await myFriend.updateOne({ $push: { applying: id } });
+  await myFriend.updateOne({ $push: { applying: target._id } });
   await targetFriend.updateOne({ $push: { waiting: token?.user?._id } });
   res.status(200).send('친구 신청을 보냈습니다.');
 });
