5534e5a72a4df24519d77daff87be227eaf3cb91
Jaeseong-CS <shimjs8@naver.com>
2020-10-13 17:26:34 +0900

    feat: friend list now returns user, not id

--- src/controllers/friend/list.ts
+++ src/controllers/friend/list.ts
@@ -2,14 +2,25 @@ import { Response } from 'express';
 
 import { IRequest, IToken } from '../../middlewares/auth';
 import Friend, { IFriend } from '../../models/Friend';
+import User, { IUser } from '../../models/User';
 
 export default async (req: IRequest, res: Response) => {
   const { token }: IToken = req;
-  const list: IFriend = await Friend.findOne({ user: token?.user?._id });
-  if (!list) {
+  const friendList: IFriend = await Friend.findOne({ user: token?.user?._id });
+  if (!friendList) {
     res.sendStatus(404);
     return;
   }
-  list.user = undefined;
+
+  const list = await Promise.all(
+    friendList.friends!.map(async (id) => {
+      const user: IUser = await User.findById(id);
+      user.admin = undefined;
+      user.password = undefined;
+      user.discord = undefined;
+      return user;
+    }),
+  );
+
   res.status(200).send(list);
 };
