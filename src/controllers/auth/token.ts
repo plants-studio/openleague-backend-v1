3cf07a8f5a49a562581f8719ad4d7083e3b86eba
Jaeseong-CS <shimjs8@naver.com>
2020-10-14 10:16:41 +0900

    fix: token struct

--- src/controllers/auth/token.ts
+++ src/controllers/auth/token.ts
@@ -27,8 +27,8 @@ export default async (req: IRequest, res: Response) => {
           return;
         }
         const verified = JSON.parse(JSON.stringify(verify(refreshToken, process.env.REFRESH_KEY!)));
-        const accessToken = sign({ user: verified.user.user }, process.env.ACCESS_KEY!);
-        const newRefreshToken = sign({ user: verified.user.user }, process.env.REFRESH_KEY!);
+        const accessToken = sign({ user: verified.user }, process.env.ACCESS_KEY!);
+        const newRefreshToken = sign({ user: verified.user }, process.env.REFRESH_KEY!);
 
         const whiteAccess: IWhitelist = new Whitelist({
           token: accessToken,
