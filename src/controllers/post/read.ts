ee98da54998b304a0173d219b5ae13f7766446eb
Jaeseong Sim <shimjs8@naver.com>
2020-11-26 16:36:35 +0900

    feat: read post method

--- src/controllers/post/read.ts
+++ src/controllers/post/read.ts
@@ -1,5 +1,19 @@
 import { Request, Response } from 'express';
 
-export default async (req: Request, res: Response) => {
+import Post from '../../models/Post';
 
+export default async (req: Request, res: Response) => {
+  const { id } = req.params;
+  if (!id) {
+    res.sendStatus(412);
+    return;
+  }
+
+  const post = await Post.findById(id);
+  if (!post) {
+    res.sendStatus(404);
+    return;
+  }
+
+  res.status(200).send(post);
 };
